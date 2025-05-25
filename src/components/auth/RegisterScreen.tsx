a77dec9613ede57543980e1654a17b591d51716d
Antonyov <orizanovargasantony@gmail.com>
2025-05-25 13:56:27 -0500

    fish

--- src/components/auth/RegisterScreen.tsx
+++ src/components/auth/RegisterScreen.tsx
@@ -55,7 +55,7 @@ const RegisterScreen: React.FC = () => {
   }
 
   return (
-    <div className=" bg-red-600 flex flex-col items-center">
+    <div className=" bg-red-600 flex flex-col items-center min-h-screen">
       <div className="w-full bg-red-600 py-6 shadow-md">
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold text-white text-center">Registrar Cuenta</h1>
       </div>
@@ -65,9 +65,9 @@ const RegisterScreen: React.FC = () => {
         <span className="text"> Volver</span>
       </button>
 
-      <div>
-        <img src="/assets/images/background_1(2).png" className='background-image-ucv' alt="" />
-      </div>
+      {/* <div> */}
+      {/*   <img src="/assets/images/background_1(2).png" className='background-image-ucv' alt="" /> */}
+      {/* </div> */}
       
 
       {/* Contenido principal */}
