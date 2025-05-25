e8b5fd3e12d52f697ce12b15ae14516b72054be8
kfoq7 <frbrizoi@gmail.com>
2025-05-24 22:47:05 -0500

    feat: added capacitor compiler

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.unievents.app',
+  appName: '.',
+  webDir: 'dist'
+};
+
+export default config;
