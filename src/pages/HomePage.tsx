6adedfe70ee9a1a84ee3d9f0d0c093074afdf020
GRE3E <84554352+GRE3E@users.noreply.github.com>
2025-05-25 00:36:29 -0500

    fix Estructura

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import Header from '@components/common/Header/Header'
 import HomeMain from '@components/eventos/Home'
-import HomeDescription from '@components/HomeDescription'
+import HomeDescription from '@components/Home/HomeDescription'
 
 export default function HomePage() {
   return (
