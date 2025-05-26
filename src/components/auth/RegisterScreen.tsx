8bf98f0fb4cc67c46098a41a080858f0bdfe6291
Alexis <alexissanchrodr@gmail.com>
2025-05-26 18:56:27 -0500

    Changed URL register

--- src/components/auth/RegisterScreen.tsx
+++ src/components/auth/RegisterScreen.tsx
@@ -26,7 +26,7 @@ const RegisterScreen: React.FC = () => {
 
     // Aquí iría la lógica de registro - Reemplazar con llamada a la API
     try {
-      const response = await fetch('https://student-events-backend-kypp.onrender.com/api/user', {
+      const response = await fetch('https://student-events-backend-kypp.onrender.com/api/auth/register', {
         // Use fetch API
         method: 'POST',
         headers: {
