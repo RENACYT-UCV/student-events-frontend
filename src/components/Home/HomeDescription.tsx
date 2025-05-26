c7a5bfda3e070670f39466368c7935ab32c374bc
dachugo <angelcampospezo7@outlook.es>
2025-05-25 20:44:11 -0500

    Implementación del Home con nueva version inicial con estilos responsive

--- src/components/Home/HomeDescription.tsx
+++ src/components/Home/HomeDescription.tsx
@@ -5,23 +5,29 @@ const HomeDescription: React.FC = () => {
   return (
     <div className="home-body-container">
       <div className="home-header-wrapper">
-        <img
-          src="/assets/images/userFrame.svg"
-          alt="Fondo perfil"
-          className="background-home-image"
-        />
+        
 
         <div className="home-header">
           <div className="home-content">
-            <div className="home-text">
-              <h1>
-                Hola, <span id="user-name">XXXXXXXX</span>
-              </h1>
-            </div>
+            <img
+              src="/assets/images/unieventos_logo_main.png"
+              alt=""
+              className="select-none left-0 object-cover z-0
+                        h-32 sm:h-40 md:h-48 lg:h-55
+                        w-auto"
+            />
 
-            <h1 className="home-subtext">Qué haremos hoy?</h1>
-          </div>
+              <div className="home-text ">
+                <h1>
+                  Bienvenido, <span id="user-name-home">user</span> !
+                </h1>
+              </div>
+
+              <h1 className="home-subtext">Qué es lo que haremos hoy?</h1>
+
+            </div>
         </div>
+        
       </div>
     </div>
   )
